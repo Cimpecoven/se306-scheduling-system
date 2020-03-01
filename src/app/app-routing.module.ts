77d45aa0cc790cf9d4791b1a1b325d82857cd16d
Cimpecoven <cimpecoven@gmail.com>
2020-03-01 14:45:40 -0600

    Added primary components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { AboutComponent } from './about/about.component';
+import { AccountComponent } from './account/account.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'account', component: AccountComponent},];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
